2d9bc7cf7e01739ac81b313383d8bcbba3fffa9c
Nuno Ramalho <nuno.miguel.ramalho@gmail.com>
2018-06-28 16:01:59 +0100

    fix: update range

--- src/test/java/examples/precisiondouble/DoubleRangeInputScannerTest.java
+++ src/test/java/examples/precisiondouble/DoubleRangeInputScannerTest.java
@@ -8,7 +8,7 @@ public class DoubleRangeInputScannerTest {
     public static void main(String[] args) {
 
         Prompt prompt = new Prompt(System.in, System.out);
-        DoubleRangeInputScanner scanner = new DoubleRangeInputScanner(3, 6);
+        DoubleRangeInputScanner scanner = new DoubleRangeInputScanner(3.5, 6.5);
 
         System.out.println(prompt.getUserInput(scanner));
 
